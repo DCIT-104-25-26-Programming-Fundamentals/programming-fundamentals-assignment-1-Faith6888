a078da4e9fcaf8aa84e4dd0335f40d0b32abcbfa
Faith6888 <faharuna003@st.ug.edu.gh>
2026-07-27 14:32:24 +0000

    Delete calculator.py

--- calculator.py
+++
@@ -1,9 +0,0 @@
-#basic calculator performing operations. 
-#z can be modified to be addition subtraction, division, ...
-
-x = float(input("What's x? "))
-y = float(input("What's y? "))
-
-z = round(x/y, 2)
-
-print(z)
